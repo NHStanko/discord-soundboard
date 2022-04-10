13ac9bfdad1f5c2a6819fc56b2f56bafbfac356e
Nick Stanko <nhstanko@gmail.com>
2022-04-10 17:02:18 -0500

    Fixing Status

--- src/commands/status.ts
+++ src/commands/status.ts
@@ -8,18 +8,39 @@ export const status: Command = {
     // Execute happens on the command
     name: "status",
     execute: async (interaction) => {
-        logger.debug(interaction);
-        client.user.setPresence({
-            activities: [
-                {name: interaction.options.getString("activity")},
-                {type: interaction.options.getString("type")},
-                {url: interaction.options.getString("url")}
-            ],
+        // Check if url string is youtube or twitch link
+        const url = interaction.options.getString("url");
+        // Check if url is a youtube or twitch link
+        if(url){
+            if(!(interaction.options.getString("type") === "streaming")){
+                interaction.reply({
+                    content: "Can only include a url if you are streaming!",
+                    ephemeral: true
+                });
+                return;
+            }else if(!(url.includes("youtube.com") || url.includes("twitch.tv"))){ 
+                interaction.reply({
+                    content: "Invalid url! Include twitch or youtube link",
+                    ephemeral: true
+                });
+                return;
+            }   
+        }
+
+        try{
+            const output = client.user.setActivity({
+                    name: interaction.options.getString("activity"),
+                    type: interaction.options.getString("type"),
+                    url: interaction.options.getString("url")
         });
+        logger.debug(output);
+        } catch (e) {
+            if(e) logger.error(`Error encountered at command execution:${interaction.commandName} - ${interaction.message} - ${e}`);
+        }
 
 
         interaction.reply({
-            content: "Pong!",
+            content: "Set Status!",
             ephemeral: true
         });
     },
